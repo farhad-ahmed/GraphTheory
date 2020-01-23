d6f8b9d353eb02f63172b1f725156de88efd2237
farhad-ahmed <frhd.ahmd@gmail.com>
2020-01-22 21:33:45 -0600

    add depth first search

---
+++ src/depthfirstsearch/DepthFirstSearch.java
@@ -0,0 +1,52 @@
+package depthfirstsearch;
+
+import java.util.*;
+
+public class DepthFirstSearch {
+
+    private List<Integer> visitedNode;
+    private List<Boolean> visited;
+    private Stack<Integer> stack;
+    private Map<Integer,List<Integer>> adjacencyMap;
+
+    public DepthFirstSearch(Map<Integer, List<Integer>> adjacencyMap){
+
+        this.adjacencyMap = adjacencyMap;
+        this.stack = new Stack<>();
+        this.visitedNode = new ArrayList<>();
+        this.visited = new ArrayList<>();
+
+        for(int i=0; i<=adjacencyMap.size()+1; i++){
+            visited.add(false);
+        }
+    }
+
+    public void generateDepthFirstSearch(Integer source){
+
+        visit(source);
+        while(!stack.empty()){
+            Integer pop = stack.pop();
+            Optional<Integer> next = findUnvisitedNode(pop);
+            if(next.isPresent()){
+                stack.push(pop);
+                visit(next.get());
+            }
+        }
+        visitedNode.stream().forEach(n -> System.out.print(n+"\t"));
+    }
+
+    private void visit(Integer source){
+
+        if(!visited.get(source)){
+            visitedNode.add(source);
+            visited.set(source, true);
+            stack.push(source);
+        }
+    }
+
+    private Optional<Integer> findUnvisitedNode(Integer source){
+
+        List<Integer> neighbors = adjacencyMap.get(source);
+        return neighbors.stream().filter(n -> !visited.get(n)).findFirst();
+    }
+}
